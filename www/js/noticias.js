369f6e7fb2b6233cb4cf057953bebeb12dda9745
NelsonNV <nelsonluisnv2@gmail.com>
2022-11-13 23:23:10 -0300

    primera injetcion de datos

--- www/js/noticias.js
+++ www/js/noticias.js
@@ -12,3 +12,52 @@ function fnGetNoticias(){
         }
     }
 }
+
+function fnPrintDivColum(noticia){
+    /*Funcion que imprime en lista*/ 
+    document.getElementById('articulo').innerHTML = noticia;
+}
+
+function fnColumnas() {
+    let noticias = dicNoticia
+    let columna = ''
+
+    const stylebutton = 'button is-large icon';
+    for (let articulo = 0; articulo < noticias.noticias.length; articulo++) {
+        const item = noticias.noticias[articulo];
+        columna += `
+            <div class="column is-3-desktop is-full-mobile Anime">
+
+                <div class="card">
+                <div class="card-content">
+                <p class="title">
+                    ${item.titulo}
+                </p>
+                </div>
+                <footer class="card-footer">
+                <p class="card-footer-item">
+                    <span>
+                    View on <a href="${item.url}">Twitter</a>
+                    </span>
+                </p>
+                <p class="card-footer-item">
+                    <span>
+                    Share on <a href="#">Facebook</a>
+                    </span>
+                </p>
+                </footer>
+            </div>
+            
+            </div>
+            ` 
+    }
+    fnPrintDivColum(columna)
+}
+
+$(document).ready(function () {
+    fnGetNoticias()
+    setTimeout(function(){
+       fnColumnas()
+    }, 5000);
+});
+
